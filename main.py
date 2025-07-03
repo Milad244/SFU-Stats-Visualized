b130c91ba050fdb0326d4392bdb266cd7bc4f016
Milad Karel Abdi <miladabdi@Mac.hitronhub.home>
2025-07-02 17:39:18 -0700

    Small progress with headcount graphs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('TKAgg')
 import pandas as pd
+import numpy as np
 
 """Data from https://www.sfu.ca/irp/students.html#outcomes"""
 
@@ -20,23 +23,39 @@ class Program:
         self.total_count = self.man_count + self.women_count + self.na_count
 
 
-def create_graph(programs: list[Program]):
-    programs.sort(key=lambda l: l.total_count)
-
-    plt.xlabel("Program")
-    plt.ylabel("Headcount")
-
-    x = [p.program for p in programs]
-    y = [p.total_count for p in programs]
-
+def create_bar_graph(x_values: list, y_values: list, x_lbl: str, y_lbl: str) -> None:
+    plt.xlabel(x_lbl)
+    plt.ylabel(y_lbl)
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.subplots_adjust(bottom=0.3)
-    plt.bar(x, y)
+    plt.bar(x_values, y_values)
     plt.show()
 
 
-def get_sfu_programs() -> list[Program]:
+def create_grouped_bar_graph(x_values: list, y_values: list, x_lbl: str, y_lbl: str) -> None:
+    pass
+
+
+def graph_programs(programs: list[Program]) -> None:
+    # Total count graph
+    programs.sort(key=lambda l: l.total_count)
+    x = [p.program for p in programs]
+    y = [p.total_count for p in programs]
+    create_bar_graph(x, y, x_lbl="Program", y_lbl="Total Count")
+    # Man count graph
+    programs.sort(key=lambda l: l.man_count)
+    x = [p.program for p in programs]
+    y = [p.man_count for p in programs]
+    create_bar_graph(x, y, x_lbl="Program", y_lbl="Man Count")
+    # Women count graph
+    programs.sort(key=lambda l: l.women_count)
+    x = [p.program for p in programs]
+    y = [p.women_count for p in programs]
+    create_bar_graph(x, y, x_lbl="Program", y_lbl="Women Count")
+
+
+def get_sfu_programs() -> None:
     stat_file = "data/headcount/ST04.db.xlsx"
     stats = pd.read_excel(stat_file, sheet_name="pivot table by gender", header=11, usecols="A:E")
 
@@ -57,9 +76,9 @@ def get_sfu_programs() -> list[Program]:
         programs.append(Program(s["Faculty"], s["Program"], s["Men"], s["Women"], s["Not reported"]))
         print(programs[-1].__dict__)
 
-    return programs
+    graph_programs(programs)
 
 
 if __name__ == '__main__':
-    create_graph(get_sfu_programs())
+    get_sfu_programs()
 
