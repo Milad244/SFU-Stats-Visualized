96a44523de07b68a81c033340de4f1cb00a179ab
Milad244 <milad.abdi244@gmail.com>
2025-07-02 21:07:43 -0700

    Got flask working!

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use('TKAgg')
 import pandas as pd
 import numpy as np
+from flask import Flask, render_template
+import plotly.express as px
+
+matplotlib.use('TKAgg')
+app = Flask(__name__)
+
 
 """Data from https://www.sfu.ca/irp/students.html#outcomes"""
 
@@ -14,13 +19,13 @@ def nan_to_int(num: int) -> int:
 
 
 class Program:
-    def __init__(self, faculty: str, program: str, man_count: int, women_count: int, na_count: int):
+    def __init__(self, faculty: str, program: str, men_count: int, women_count: int, nr_count: int):
         self.faculty = faculty
         self.program = program
-        self.man_count = nan_to_int(man_count)
+        self.men_count = nan_to_int(men_count)
         self.women_count = nan_to_int(women_count)
-        self.na_count = nan_to_int(na_count)
-        self.total_count = self.man_count + self.women_count + self.na_count
+        self.nr_count = nan_to_int(nr_count)
+        self.total_count = self.men_count + self.women_count + self.nr_count
 
 
 def create_bar_graph(x_values: list, y_values: list, x_lbl: str, y_lbl: str) -> None:
@@ -33,10 +38,6 @@ def create_bar_graph(x_values: list, y_values: list, x_lbl: str, y_lbl: str) -> 
     plt.show()
 
 
-def create_grouped_bar_graph(x_values: list, y_values: list, x_lbl: str, y_lbl: str) -> None:
-    pass
-
-
 def graph_programs(programs: list[Program]) -> None:
     # Total count graph
     programs.sort(key=lambda l: l.total_count)
@@ -44,10 +45,10 @@ def graph_programs(programs: list[Program]) -> None:
     y = [p.total_count for p in programs]
     create_bar_graph(x, y, x_lbl="Program", y_lbl="Total Count")
     # Man count graph
-    programs.sort(key=lambda l: l.man_count)
+    programs.sort(key=lambda l: l.men_count)
     x = [p.program for p in programs]
-    y = [p.man_count for p in programs]
-    create_bar_graph(x, y, x_lbl="Program", y_lbl="Man Count")
+    y = [p.men_count for p in programs]
+    create_bar_graph(x, y, x_lbl="Program", y_lbl="Men Count")
     # Women count graph
     programs.sort(key=lambda l: l.women_count)
     x = [p.program for p in programs]
@@ -55,7 +56,7 @@ def graph_programs(programs: list[Program]) -> None:
     create_bar_graph(x, y, x_lbl="Program", y_lbl="Women Count")
 
 
-def get_sfu_programs() -> None:
+def get_sfu_programs() -> list[Program]:
     stat_file = "data/headcount/ST04.db.xlsx"
     stats = pd.read_excel(stat_file, sheet_name="pivot table by gender", header=11, usecols="A:E")
 
@@ -76,9 +77,26 @@ def get_sfu_programs() -> None:
         programs.append(Program(s["Faculty"], s["Program"], s["Men"], s["Women"], s["Not reported"]))
         print(programs[-1].__dict__)
 
-    graph_programs(programs)
+    # graph_programs(programs)
+    return programs
+
+
+@app.route("/")
+def main():
+    programs = get_sfu_programs()
+    programs.sort(key=lambda l: l.total_count)
+    x = [p.program for p in programs]
+    y = [p.total_count for p in programs]
+    df = pd.DataFrame({
+        "Program": x,
+        "Headcount": y
+    })
+
+    fig = px.bar(df, x="Program", y="Headcount", title="Total Headcount by Program")
+
+    graph_html = fig.to_html(full_html=False)
+    return render_template("index.html", stat="Total Headcount by Program", graph_html=graph_html)
 
 
 if __name__ == '__main__':
-    get_sfu_programs()
-
+    app.run(debug=True, port=8000)
